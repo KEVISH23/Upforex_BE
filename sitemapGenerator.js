1800ea417f7ffb2912a5227089059d6dc1d3986f
KEVISH23 <79779725+KEVISH23@users.noreply.github.com>
2025-06-23 21:40:56 +0530

    fix: added all routes

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -10,8 +10,61 @@ const BASE_URL = process.env.BASE_URL || "https://upforex.com";
 export async function generateSitemap() {
   const links = [
     { url: "/", changefreq: "daily", priority: 1.0 },
-    { url: "/about", changefreq: "monthly", priority: 0.7 },
-    { url: "/contact", changefreq: "monthly", priority: 0.7 },
+    { url: "/about-us", changefreq: "monthly", priority: 0.8 },
+    { url: "/contact-us", changefreq: "monthly", priority: 0.8 },
+    { url: "/risk-disclosure", changefreq: "yearly", priority: 0.5 },
+    { url: "/anti-monry-laundering", changefreq: "yearly", priority: 0.5 },
+    { url: "/privacy-policy", changefreq: "yearly", priority: 0.5 },
+    { url: "/blogs", changefreq: "weekly", priority: 0.9 },
+
+    // Products
+    { url: "/products/forex", changefreq: "weekly", priority: 0.7 },
+    { url: "/products/us-stocks", changefreq: "weekly", priority: 0.7 },
+    { url: "/products/precious-metal", changefreq: "weekly", priority: 0.7 },
+    { url: "/products/index-future", changefreq: "weekly", priority: 0.7 },
+    { url: "/products/energy-future", changefreq: "weekly", priority: 0.7 },
+    {
+      url: "/products/cryptocurrencies-cfd",
+      changefreq: "weekly",
+      priority: 0.7,
+    },
+
+    // Resources
+    {
+      url: "/resources/introduction-to-forex",
+      changefreq: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "/resources/trading-glossary",
+      changefreq: "monthly",
+      priority: 0.6,
+    },
+    { url: "/resources/trading-rules", changefreq: "monthly", priority: 0.6 },
+    {
+      url: "/resources/contract-specifications",
+      changefreq: "monthly",
+      priority: 0.6,
+    },
+
+    // Platforms
+    { url: "/platforms/mt5", changefreq: "monthly", priority: 0.7 },
+
+    // Partnerships
+    { url: "/partnerships", changefreq: "monthly", priority: 0.7 },
+    {
+      url: "/partnerships/social-trading",
+      changefreq: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "/partnerships/education-partners",
+      changefreq: "monthly",
+      priority: 0.7,
+    },
+
+    // Accounts
+    { url: "/accounts", changefreq: "monthly", priority: 0.7 },
   ];
 
   // Fetch blogs for dynamic URLs
