9f05db45f6fa4df07fe1214a9cefa168aa0e2526
ITsoftexpert <75052372+ITsoftexpert@users.noreply.github.com>
2025-07-14 14:49:07 +0000

    add T&ccc

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 export * from "./adminSchema.js";
 export * from "./blogSchema.js";
 export * from "./categorySchema.js";
-export * from "./termsSchema.js";
-export * from "./userAcceptanceSchema.js";
+// export * from "./termsSchema.js";
+// export * from "./userAcceptanceSchema.js";
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
 export * from "./adminRoutes.js";
 export * from "./blogRoutes.js";
 export * from "./categoriesRoutes.js";
-export * from "./termsRoutes.js";
+// export * from "./termsRoutes.js";
