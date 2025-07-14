a47c6781712a5749afb68282cadca0f7f95e9641
ITsoftexpert <75052372+ITsoftexpert@users.noreply.github.com>
2025-07-14 15:09:58 +0000

    remove termsRoutess

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import * as nodemailer from "./nodemailer.js";
 import dotenv from "dotenv";
 import cors from "cors";
-import { adminRouter, blogRouter, categoryRouter } from "./routes/index.js";import axios from "axios";
+import { adminRouter, blogRouter, categoryRouter } from "./routes/index.js";
+import axios from "axios";
 import "./dbConnect.js";
 import { generateSitemap } from "./sitemapGenerator.js";
 dotenv.config();
@@ -19,8 +20,6 @@ app.listen(process.env.PORT, () => {
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/blogs", blogRouter);
 app.use("/api/v1/categories", categoryRouter);
-// app.use("/api/v1/terms", termsRouter);
-
 // app.post("/api/v1/upforex/contact-us", async (req, res) => {
 //   try {
 //     await nodemailer.sendMail(req.body);
